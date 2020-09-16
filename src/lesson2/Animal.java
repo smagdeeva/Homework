743d71815636fcdac5bb4a57f0b83371102aa782
smagdeeva <kendraniht@gmail.com>
2020-09-16 19:51:25 +0300

    2 задание-1

--- src/lesson2/Animal.java
+++ src/lesson2/Animal.java
@@ -1,4 +1,26 @@
 package lesson2;
 
 public class Animal {
+
+    String food;
+    String location;
+
+    public Animal(String food, String location) {
+        this.food = food;
+        this.location = location;
+    }
+
+    protected void makeNoise() {
+        System.out.println("");
+    }
+
+    protected void eat() {
+        System.out.println("");
+    }
+
+    protected void sleep() {
+        System.out.println("");
+    }
 }
+
+
